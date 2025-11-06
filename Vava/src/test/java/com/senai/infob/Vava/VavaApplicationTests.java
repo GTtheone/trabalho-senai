d450fa5b6ec94aaaa1719f19bd6e0712e5ef3bcb
Gustavo Enrique de Souza Martins e Souza <gusupp09@gmail.com>
2025-11-06 09:32:17 -0300

    Add files via upload

---
+++ Vava/src/test/java/com/senai/infob/Vava/VavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.infob.Vava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
